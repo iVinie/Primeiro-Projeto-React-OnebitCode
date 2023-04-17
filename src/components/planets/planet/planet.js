73453a68e8e9cd43f6b59469342ca8b956675ecd
iVinie <vinicius.dsro@hotmail.com>
2023-04-17 11:41:23 -0300

    Refatorando os states e lifecycle com hooks

--- src/components/planets/planet/planet.js
+++ src/components/planets/planet/planet.js
@@ -1,48 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ImgPlanet from '../../../shared/img/image'
 import Description from '../../../shared/description/description'
 import Title from "../../../shared/title/title"
 import '../../../css/stylePlanet.css'
 import Satellites from '../../../shared/satellites/Satellites'
-
 async function getSatellites(id) {
     let res = await fetch(`http://localhost:3000/api/${id}.json`)
     let date = await res.json()
     return date
 }
-class Planet extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            satellites: []
-        }
-    }
-    componentDidMount() {
-        getSatellites(this.props.id).then(date => {
-            this.setState(state => ({
-                satellites: date['satellites']
-            }))
+const Planet = (props) => {
+    const [satellites, setSatellites] = useState([])
+    useEffect(() => {
+        getSatellites(props.id).then(date => {
+            setSatellites(date['satellites'])
         })
-    }
-    render() {
-        return (
-            <div className="test" onClick={() => { this.props.ClikPlanet(this.props.name) }}>
-                <Title name={this.props.name} />
-                <Description description={this.props.description} link={this.props.link} />
-                <ImgPlanet img_url={this.props.img_url} />
-                <p className="title">Satellites</p>
-                <ul>
-                    {
-                        this.state.satellites.map((satellite, i) =>
-                            <Satellites 
-                            name={satellite.name} 
+    }, [])
+    return (
+        <div className="test" onClick={() => { props.ClikPlanet(props.name) }}>
+            <Title name={props.name} />
+            <Description description={props.description} link={props.link} />
+            <ImgPlanet img_url={props.img_url} />
+            <p className="title">Satellites</p>
+            <ul>
+                {
+                    satellites.map((satellite, i) =>
+                        <Satellites
+                            name={satellite.name}
                             key={i}
-                            />
-                        )
-                    }
-                </ul>
-            </div>
-        )
-    }
+                        />
+                    )
+                }
+            </ul>
+        </div>
+    )
 }
 export default Planet
--- src/components/planets/planets.js
+++ src/components/planets/planets.js
@@ -1,54 +1,39 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 import Planet from "./planet/planet"
 import '../../css/stylePlanets.css'
-
-async function getPlanets(){
-   /*  let res = await fetch('http://localhost:3000/api/planets.json') */
-   let res = await fetch('http:')
+async function getPlanets() {
+    let res = await fetch('http://localhost:3000/api/planets.json')
     let date = await res.json()
     return date
 }
-
-
 const ClikPlanet = (name) => {
     console.log(`Clicou no: ${name}`)
 }
-class Planets extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            planets: []
-        }
-    }
-    componentDidMount(){
-        getPlanets().then(date =>{
-            this.setState(state => ({
-                planets: date['planets']
-            }))
+const Planets = () => {
+    const [planets, setPlanets] = useState([])
+    useEffect(() => {
+        getPlanets().then(date => {
+            setPlanets(date['planets'])
         })
-    }
-    render() {
-        return (
-            <Fragment>
-                <h1>Planet List</h1>
-                <hr></hr>
-                {
-                    this.state.planets.map((planet, index) => 
-                        <Planet
-                            name={planet.name}
-                            description={planet.description}
-                            img_url={planet.img_url}
-                            link={planet.link}
-                            ClikPlanet={ClikPlanet}
-                            id={planet.id}
-                            key={index}
-                        />
-                    )
-                }
-            </Fragment>
-        )
-    }
+    }, [])
+    return (
+        <Fragment>
+            <h1>Planet List</h1>
+            <hr></hr>
+            {
+                planets.map((planet, index) =>
+                    <Planet
+                        name={planet.name}
+                        description={planet.description}
+                        img_url={planet.img_url}
+                        link={planet.link}
+                        ClikPlanet={ClikPlanet}
+                        id={planet.id}
+                        key={index}
+                    />
+                )
+            }
+        </Fragment>
+    )
 }
-
-
 export default Planets
